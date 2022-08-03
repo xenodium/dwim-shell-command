6ba139a12ac1dad2ae8447810d01cef130e53f46
xenodium <me+gh@xenodium.com>
2022-08-03 19:37:33 +0100

    Updating commentary

--- dwim-shell-command.el
+++ dwim-shell-command.el
@@ -27,6 +27,8 @@
 ;;
 ;; Use `dwim-shell-command-on-marked-files' to create your own command
 ;; line utilities, invoked via M-x.
+;;
+;; See examples at https://github.com/xenodium/dwim-shell-command/blob/main/dwim-shell-commands.el
 
 ;;; Code:
 
