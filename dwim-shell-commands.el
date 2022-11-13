b98f45c7901446cf1ab60be2ab648c623e774427
xenodium <me+gh@xenodium.com>
2022-11-13 09:04:27 +0000

    Adds dwim-shell-commands-kill-process

--- dwim-shell-commands.el
+++ dwim-shell-commands.el
@@ -29,6 +29,7 @@
 (require 'cl-lib)
 (require 'dwim-shell-command)
 (require 'files)
+(require 'proced)
 (require 'seq)
 (require 'subr-x)
 
@@ -286,6 +287,49 @@ Optional argument ARGS as per `browse-url-default-browser'"
    "pandoc -t latex '<<f>>' -o '<<fne>>.pdf'"
    :extensions "docx" ;; brew install mactex
    :utils "pdflatex"))
+
+(defun dwim-shell-commands-kill-process ()
+  "Select and kill process."
+  (interactive)
+  (let* ((pid-width 5)
+         (comm-width 25)
+         (user-width 10)
+         (processes (proced-process-attributes))
+         (candidates
+          (mapcar (lambda (attributes)
+                    (let* ((process (cdr attributes))
+                           (pid (format (format "%%%ds" pid-width) (map-elt process 'pid)))
+                           (user (format (format "%%-%ds" user-width)
+                                         (truncate-string-to-width
+                                          (map-elt process 'user) user-width nil nil t)))
+                           (comm (format (format "%%-%ds" comm-width)
+                                         (truncate-string-to-width
+                                          (map-elt process 'comm) comm-width nil nil t)))
+                           (args-width (- (window-width) (+ pid-width user-width comm-width 3)))
+                           (args (map-elt process 'args)))
+                      (cons (if args
+                                (format "%s %s %s %s" pid user comm (truncate-string-to-width args args-width nil nil t))
+                              (format "%s %s %s" pid user comm))
+                            process)))
+                  processes))
+         (selection (map-elt candidates
+                             (completing-read "kill process: "
+                                              (seq-sort
+                                               (lambda (p1 p2)
+                                                 (string-lessp (nth 2 (split-string (string-trim (car p1))))
+                                                               (nth 2 (split-string (string-trim (car p2))))))
+                                               candidates) nil t)))
+         (prompt-title (format "%s %s %s"
+                               (map-elt selection 'pid)
+                               (map-elt selection 'user)
+                               (map-elt selection 'comm))))
+    (when (y-or-n-p (format "Kill? %s" prompt-title))
+      (dwim-shell-command-on-marked-files
+       (format "Kill %s" prompt-title)
+       (format "kill -9 %d" (map-elt selection 'pid))
+       :utils "kill"
+       :error-autofocus t
+       :silent-success t))))
 
 (defun dwim-shell-commands-macos-toggle-dark-mode ()
   "Toggle macOS dark mode."
